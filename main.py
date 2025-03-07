738e12fa8c1ee1c63ccf04cf474e3376ee443768
mitisit <29955395+mitisit89@users.noreply.github.com>
2025-03-07 21:26:36 +0300

    feat:add main

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from secrtes-keeper!")
+from fastapi import FastAPI
+from app.api.router import router
+from app.settings import settings
+from app.db.connection import engine
+from app.db.models import ServicePassword
+
+app = FastAPI(title=settings.APP_NAME)
+app.include_router(router)
+
+
+@app.on_event("startup")
+async def startup():
+    async with engine.begin() as conn:
+        await conn.run_sync(ServicePassword.metadata.create_all)
 
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
