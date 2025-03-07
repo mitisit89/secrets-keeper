0bffae2a86f1768195a65a0c23ae561b3481d03c
mitisit <29955395+mitisit89@users.noreply.github.com>
2025-03-07 21:26:09 +0300

    feat:add settings

--- app/settings.py
+++ app/settings.py
@@ -2,12 +2,13 @@ import os
 
 from cryptography.fernet import Fernet
 from pydantic_settings import BaseSettings
+from pathlib import Path
 
 
 class _Settings(BaseSettings):
     APP_NAME: str = "Secrtes Keeper"
     SQLALCHEMY_DATABASE_URL: str = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///./test.db")
-    SECRET_KEY: str = os.getenv("SECRET_KEY", Fernet.generate_key().decode())
+    SECRET_KEY: str = Path("secret.key").read_text()
 
 
 settings = _Settings()
